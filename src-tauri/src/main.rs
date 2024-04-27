14efae78f748e7a0b0783302190e2717ae44973c
hollowdll <pixerchat@gmail.com>
2024-04-27 21:55:42 +0300

    add initial menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, Menu, Submenu};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
   format!("Hello, {}!", name)
@@ -8,6 +10,19 @@ fn greet(name: &str) -> String {
 
 fn main() {
   tauri::Builder::default()
+    .menu(Menu::os_default("kvdb-gui")
+      .add_submenu(Submenu::new("Connect", Menu::with_items([
+        CustomMenuItem::new("new-connection", "New kvdb connection").into(),
+        CustomMenuItem::new("disconnect", "Disconnect").into(),
+      ])))
+    )
+    .on_menu_event(|event| {
+      match event.menu_item_id() {
+        "new-connection" => println!("Menu event -> New kvdb connection"),
+        "disconnect" => println!("Menu event -> Disconnect"),
+        _ => {}
+      }
+    })
     .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
