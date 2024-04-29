9cc9a3b1a97bfcb5aee099547b87a6938f1746c6
hollowdll <pixerchat@gmail.com>
2024-04-29 21:11:23 +0300

    grpc client generation

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,14 @@
-fn main() {
-  tauri_build::build()
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  tauri_build::build();
+  tonic_build::configure()
+    .build_server(false)
+    .compile(
+      &[
+        "../proto/kvdbserverpb/db.proto",
+        "../proto/kvdbserverpb/server.proto",
+        "../proto/kvdbserverpb/storage.proto"
+      ],
+      &["../proto/kvdbserverpb"],
+    )?;
+  Ok(())
 }
