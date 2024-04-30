b5e178dcb0e48e85a6773ae860b6b2e53690a258
hollowdll <pixerchat@gmail.com>
2024-04-30 22:30:25 +0300

    init grpc connection state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,37 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{CustomMenuItem, Menu, Submenu};
+use std::error::Error;
+use app::grpc::GrpcClient;
+use tauri::{CustomMenuItem, Menu, Submenu, State, Manager};
+use std::sync::Mutex;
+
+struct GrpcConnection {
+  connection: Mutex<Option<GrpcClient>>,
+}
+
+impl GrpcConnection {
+  pub fn new() -> GrpcConnection {
+    GrpcConnection {
+      connection: None.into(),
+    }
+  }
+}
 
 #[tauri::command]
 fn greet(name: &str) -> String {
   format!("Hello, {}!", name)
 }
 
-fn main() {
+/*
+#[tauri::command]
+fn connect(connection: State<GrpcConnection>, host: &str, port: u16) -> Result<String, String> {
+
+}
+*/
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
   tauri::Builder::default()
     .menu(Menu::os_default("kvdb-gui")
       .add_submenu(Submenu::new("Connect", Menu::with_items([
@@ -26,7 +49,14 @@ fn main() {
         _ => {}
       }
     })
+    .setup(|app| {
+      app.manage(GrpcConnection::new());
+
+      Ok(())
+    })
     .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
+  Ok(())
 }
