107bd88de6efc6cfa747ff56c62fd892fc355d78
hollowdll <pixerchat@gmail.com>
2024-05-23 20:55:05 +0300

    server info accordion improvements

--- src/components/server/InfoTabPanel.tsx
+++ src/components/server/InfoTabPanel.tsx
@@ -8,113 +8,117 @@ import {
   Typography,
 } from "@mui/material";
 import { useServerInfoStore } from "../../state/store";
-import { ClientInfo, MemoryInfo, StorageInfo, GeneralInfo } from "../../types/server";
+import {
+  ClientInfo,
+  MemoryInfo,
+  StorageInfo,
+  GeneralInfo,
+} from "../../types/server";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useState, SyntheticEvent } from "react";
 
+type AccordionProps = {
+  accordionExpanded: string | false;
+  handleAccordionChange: (
+    panel: string
+  ) => (_event: SyntheticEvent, isExpanded: boolean) => void;
+};
+
 type MemoryInfoListProps = {
-  info: MemoryInfo,
+  info: MemoryInfo;
+  accordion: AccordionProps;
 };
 
 type StorageInfoListProps = {
-  info: StorageInfo,
+  info: StorageInfo;
+  accordion: AccordionProps;
 };
 
 type ClientInfoListProps = {
-  info: ClientInfo,
-}
+  info: ClientInfo;
+  accordion: AccordionProps;
+};
 
 type GeneralInfoListProps = {
-  info: GeneralInfo,
-}
+  info: GeneralInfo;
+  accordion: AccordionProps;
+};
 
 function allyPropsInfoValue(infoField: string) {
   return {
     id: `server-info-value-${infoField}`,
-    sx: { 'marginRight': '30px', textAlign: "end", color: "text.secondary" },
+    sx: { marginRight: "30px", textAlign: "end", color: "text.secondary" },
   };
 }
 
-function MemoryInfoList({ info }: MemoryInfoListProps) {
-  const [accordionExpanded, setAccordionExpanded] = useState<string | false>(
-    false
-  );
-
-  const handleAccordionChange =
-    (panel: string) => (_event: SyntheticEvent, isExpanded: boolean) => {
-      setAccordionExpanded(isExpanded ? panel : false);
-    };
+function MemoryInfoList({ info, accordion }: MemoryInfoListProps) {
+  const { accordionExpanded, handleAccordionChange } = accordion;
 
   return (
     <Box sx={{ paddingTop: "1em" }}>
-    <h3>Memory</h3>
-    <Accordion
-      expanded={accordionExpanded === "panel1"}
-      onChange={handleAccordionChange("panel1")}
-    >
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <ListItemText primary="memory_alloc" />
-        <ListItemText
-          primary={`${Number(info.memoryAllocMegaByte).toFixed(1)} MB`}
-          {...allyPropsInfoValue("memory-alloc")}
-        />
-      </AccordionSummary>
-      <AccordionDetails>
-        <Typography>Allocated memory in megabytes.</Typography>
-      </AccordionDetails>
-    </Accordion>
+      <h3>Memory</h3>
+      <Accordion
+        expanded={accordionExpanded === "panel1"}
+        onChange={handleAccordionChange("panel1")}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <ListItemText primary="memory_alloc" />
+          <ListItemText
+            primary={`${Number(info.memoryAllocMegaByte).toFixed(1)} MB`}
+            {...allyPropsInfoValue("memory-alloc")}
+          />
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>Allocated memory in megabytes.</Typography>
+        </AccordionDetails>
+      </Accordion>
 
-    <Accordion
-      expanded={accordionExpanded === "panel2"}
-      onChange={handleAccordionChange("panel2")}
-    >
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <ListItemText primary="memory_total_alloc" />
-        <ListItemText
-          primary={`${Number(info.memoryTotalAllocMegaByte).toFixed(1)} MB`}
-          {...allyPropsInfoValue("memory-total-alloc")}
-        />
-      </AccordionSummary>
-      <AccordionDetails>
-        <Typography>Total allocated memory in megabytes.</Typography>
-      </AccordionDetails>
-    </Accordion>
+      <Accordion
+        expanded={accordionExpanded === "panel2"}
+        onChange={handleAccordionChange("panel2")}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <ListItemText primary="memory_total_alloc" />
+          <ListItemText
+            primary={`${Number(info.memoryTotalAllocMegaByte).toFixed(1)} MB`}
+            {...allyPropsInfoValue("memory-total-alloc")}
+          />
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>Total allocated memory in megabytes.</Typography>
+        </AccordionDetails>
+      </Accordion>
 
-    <Accordion
-      expanded={accordionExpanded === "panel3"}
-      onChange={handleAccordionChange("panel3")}
-    >
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <ListItemText primary="memory_sys" />
-        <ListItemText
-          primary={`${Number(info.memorySysMegaByte).toFixed(1)} MB`}
-          {...allyPropsInfoValue("memory-sys")}
-        />
-      </AccordionSummary>
-      <AccordionDetails>
-        <Typography>Total memory obtained from the OS in megabytes.</Typography>
-      </AccordionDetails>
-    </Accordion>
-  </Box>
+      <Accordion
+        expanded={accordionExpanded === "panel3"}
+        onChange={handleAccordionChange("panel3")}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <ListItemText primary="memory_sys" />
+          <ListItemText
+            primary={`${Number(info.memorySysMegaByte).toFixed(1)} MB`}
+            {...allyPropsInfoValue("memory-sys")}
+          />
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Total memory obtained from the OS in megabytes.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
   );
 }
 
-function StorageInfoList({ info }: StorageInfoListProps) {
-  const [accordionExpanded, setAccordionExpanded] = useState<string | false>(
-    false
-  );
-
-  const handleAccordionChange =
-    (panel: string) => (_event: SyntheticEvent, isExpanded: boolean) => {
-      setAccordionExpanded(isExpanded ? panel : false);
-    };
+function StorageInfoList({ info, accordion }: StorageInfoListProps) {
+  const { accordionExpanded, handleAccordionChange } = accordion;
 
   return (
     <Box sx={{ paddingTop: "1em" }}>
       <h3>Data Storage</h3>
       <Accordion
-        expanded={accordionExpanded === "panel1"}
-        onChange={handleAccordionChange("panel1")}
+        expanded={accordionExpanded === "panel4"}
+        onChange={handleAccordionChange("panel4")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="total_data_size" />
@@ -129,8 +133,8 @@ function StorageInfoList({ info }: StorageInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel2"}
-        onChange={handleAccordionChange("panel2")}
+        expanded={accordionExpanded === "panel5"}
+        onChange={handleAccordionChange("panel5")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="total_keys" />
@@ -147,22 +151,15 @@ function StorageInfoList({ info }: StorageInfoListProps) {
   );
 }
 
-function ClientInfoList({ info }: ClientInfoListProps) {
-  const [accordionExpanded, setAccordionExpanded] = useState<string | false>(
-    false
-  );
-
-  const handleAccordionChange =
-    (panel: string) => (_event: SyntheticEvent, isExpanded: boolean) => {
-      setAccordionExpanded(isExpanded ? panel : false);
-    };
+function ClientInfoList({ info, accordion }: ClientInfoListProps) {
+  const { accordionExpanded, handleAccordionChange } = accordion;
 
   return (
     <Box sx={{ paddingTop: "1em" }}>
       <h3>Clients</h3>
       <Accordion
-        expanded={accordionExpanded === "panel1"}
-        onChange={handleAccordionChange("panel1")}
+        expanded={accordionExpanded === "panel6"}
+        onChange={handleAccordionChange("panel6")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="client_connections" />
@@ -177,8 +174,8 @@ function ClientInfoList({ info }: ClientInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel2"}
-        onChange={handleAccordionChange("panel2")}
+        expanded={accordionExpanded === "panel7"}
+        onChange={handleAccordionChange("panel7")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="max_client_connections" />
@@ -188,29 +185,24 @@ function ClientInfoList({ info }: ClientInfoListProps) {
           />
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>Maximum number of active client connections allowed.</Typography>
+          <Typography>
+            Maximum number of active client connections allowed.
+          </Typography>
         </AccordionDetails>
       </Accordion>
     </Box>
   );
 }
 
-function GeneralInfoList({ info }: GeneralInfoListProps) {
-  const [accordionExpanded, setAccordionExpanded] = useState<string | false>(
-    false
-  );
-
-  const handleAccordionChange =
-    (panel: string) => (_event: SyntheticEvent, isExpanded: boolean) => {
-      setAccordionExpanded(isExpanded ? panel : false);
-    };
+function GeneralInfoList({ info, accordion }: GeneralInfoListProps) {
+  const { accordionExpanded, handleAccordionChange } = accordion;
 
   return (
     <Box>
       <h3>General</h3>
       <Accordion
-        expanded={accordionExpanded === "panel1"}
-        onChange={handleAccordionChange("panel1")}
+        expanded={accordionExpanded === "panel8"}
+        onChange={handleAccordionChange("panel8")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="kvdb_version" />
@@ -225,8 +217,8 @@ function GeneralInfoList({ info }: GeneralInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel2"}
-        onChange={handleAccordionChange("panel2")}
+        expanded={accordionExpanded === "panel9"}
+        onChange={handleAccordionChange("panel9")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="go_version" />
@@ -241,8 +233,8 @@ function GeneralInfoList({ info }: GeneralInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel3"}
-        onChange={handleAccordionChange("panel3")}
+        expanded={accordionExpanded === "panel10"}
+        onChange={handleAccordionChange("panel10")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="db_count" />
@@ -257,15 +249,12 @@ function GeneralInfoList({ info }: GeneralInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel4"}
-        onChange={handleAccordionChange("panel4")}
+        expanded={accordionExpanded === "panel11"}
+        onChange={handleAccordionChange("panel11")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="os" />
-          <ListItemText
-            primary={info.os}
-            {...allyPropsInfoValue("os")}
-          />
+          <ListItemText primary={info.os} {...allyPropsInfoValue("os")} />
         </AccordionSummary>
         <AccordionDetails>
           <Typography>Server operating system.</Typography>
@@ -273,15 +262,12 @@ function GeneralInfoList({ info }: GeneralInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel5"}
-        onChange={handleAccordionChange("panel5")}
+        expanded={accordionExpanded === "panel12"}
+        onChange={handleAccordionChange("panel12")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="arch" />
-          <ListItemText
-            primary={info.arch}
-            {...allyPropsInfoValue("arch")}
-          />
+          <ListItemText primary={info.arch} {...allyPropsInfoValue("arch")} />
         </AccordionSummary>
         <AccordionDetails>
           <Typography>Server architecture.</Typography>
@@ -289,8 +275,8 @@ function GeneralInfoList({ info }: GeneralInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel6"}
-        onChange={handleAccordionChange("panel6")}
+        expanded={accordionExpanded === "panel13"}
+        onChange={handleAccordionChange("panel13")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="process_id" />
@@ -305,8 +291,8 @@ function GeneralInfoList({ info }: GeneralInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel7"}
-        onChange={handleAccordionChange("panel7")}
+        expanded={accordionExpanded === "panel14"}
+        onChange={handleAccordionChange("panel14")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="uptime_seconds" />
@@ -321,8 +307,8 @@ function GeneralInfoList({ info }: GeneralInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel8"}
-        onChange={handleAccordionChange("panel8")}
+        expanded={accordionExpanded === "panel15"}
+        onChange={handleAccordionChange("panel15")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="tcp_port" />
@@ -337,8 +323,8 @@ function GeneralInfoList({ info }: GeneralInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel9"}
-        onChange={handleAccordionChange("panel9")}
+        expanded={accordionExpanded === "panel16"}
+        onChange={handleAccordionChange("panel16")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="default_db" />
@@ -353,8 +339,8 @@ function GeneralInfoList({ info }: GeneralInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel10"}
-        onChange={handleAccordionChange("panel10")}
+        expanded={accordionExpanded === "panel17"}
+        onChange={handleAccordionChange("panel17")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="tls_enabled" />
@@ -369,8 +355,8 @@ function GeneralInfoList({ info }: GeneralInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel11"}
-        onChange={handleAccordionChange("panel11")}
+        expanded={accordionExpanded === "panel18"}
+        onChange={handleAccordionChange("panel18")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="password_enabled" />
@@ -385,8 +371,8 @@ function GeneralInfoList({ info }: GeneralInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel12"}
-        onChange={handleAccordionChange("panel12")}
+        expanded={accordionExpanded === "panel19"}
+        onChange={handleAccordionChange("panel19")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="logfile_enabled" />
@@ -401,8 +387,8 @@ function GeneralInfoList({ info }: GeneralInfoListProps) {
       </Accordion>
 
       <Accordion
-        expanded={accordionExpanded === "panel13"}
-        onChange={handleAccordionChange("panel13")}
+        expanded={accordionExpanded === "panel20"}
+        onChange={handleAccordionChange("panel20")}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <ListItemText primary="debug_enabled" />
@@ -421,15 +407,42 @@ function GeneralInfoList({ info }: GeneralInfoListProps) {
 
 export default function InfoTabPanel() {
   const serverInfo = useServerInfoStore((state) => state.serverInfo);
+  const [accordionExpanded, setAccordionExpanded] = useState<string | false>(
+    false
+  );
+
+  const handleAccordionChange =
+    (panel: string) => (_event: SyntheticEvent, isExpanded: boolean) => {
+      setAccordionExpanded(isExpanded ? panel : false);
+    };
 
   return (
-    <Box sx={{ p: 3, backgroundColor: "rgb(250, 250, 250)", width: "100%", maxWidth: "720px" }}>
+    <Box
+      sx={{
+        p: 3,
+        backgroundColor: "rgb(250, 250, 250)",
+        width: "100%",
+        maxWidth: "720px",
+      }}
+    >
       {serverInfo ? (
         <>
-          <GeneralInfoList info={serverInfo.generalInfo} />
-          <StorageInfoList info={serverInfo.storageInfo} />
-          <ClientInfoList info={serverInfo.clientInfo} />
-          <MemoryInfoList info={serverInfo.memoryInfo} />
+          <GeneralInfoList
+            info={serverInfo.generalInfo}
+            accordion={{ accordionExpanded, handleAccordionChange }}
+          />
+          <StorageInfoList
+            info={serverInfo.storageInfo}
+            accordion={{ accordionExpanded, handleAccordionChange }}
+          />
+          <ClientInfoList
+            info={serverInfo.clientInfo}
+            accordion={{ accordionExpanded, handleAccordionChange }}
+          />
+          <MemoryInfoList
+            info={serverInfo.memoryInfo}
+            accordion={{ accordionExpanded, handleAccordionChange }}
+          />
         </>
       ) : (
         <CircularProgress />
