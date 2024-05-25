698626661d9176b392f5f4da3c98ea81131c39e3
hollowdll <pixerchat@gmail.com>
2024-05-25 16:16:49 +0300

    init database view components

--- src/components/views/DatabasesView.tsx
+++ src/components/views/DatabasesView.tsx
@@ -1,9 +1,17 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Button } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import DatabaseList from "../database/DatabaseList";
+import CreateDatabaseDialog from "../database/CreateDatabaseDialog";
 
 export default function DatabasesView() {
   return (
     <Box>
       <h1>Databases</h1>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" startIcon={<AddIcon />}>New</Button>
+      </Stack>
+      <DatabaseList />
+      <CreateDatabaseDialog />
     </Box>
   );
 }
