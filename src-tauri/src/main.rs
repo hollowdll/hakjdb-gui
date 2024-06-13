ba30b54759ab46281d0a92ab03fb5d361a40d9d1
hollowdll <pixerchat@gmail.com>
2024-06-13 21:39:31 +0300

    add get_type_of_key tauri command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,8 +11,8 @@ use app::{
     grpc::GrpcConnection,
     server::{__cmd__get_server_info, __cmd__get_server_logs, get_server_info, get_server_logs},
     storage::{
-        __cmd__delete_all_keys, __cmd__delete_key, __cmd__get_keys, __cmd__get_string,
-        __cmd__set_string, delete_all_keys, delete_key, get_keys, get_string, set_string,
+        __cmd__delete_all_keys, __cmd__delete_key, __cmd__get_keys, __cmd__get_type_of_key, __cmd__get_string,
+        __cmd__set_string, delete_all_keys, delete_key, get_keys, get_type_of_key, get_string, set_string,
     },
 };
 use std::error::Error;
@@ -54,6 +54,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             create_database,
             delete_database,
             get_keys,
+            get_type_of_key,
             get_string,
             set_string,
             delete_key,
--- src-tauri/src/storage.rs
+++ src-tauri/src/storage.rs
@@ -2,7 +2,7 @@ use crate::{
     error::NO_CONNECTION_FOUND_MSG,
     grpc::{
         kvdb::{
-            DeleteAllKeysRequest, DeleteKeyRequest, GetKeysRequest, GetStringRequest,
+            DeleteAllKeysRequest, DeleteKeyRequest, GetKeysRequest, GetStringRequest, GetTypeOfKeyRequest,
             SetStringRequest,
         },
         GrpcConnection, MD_KEY_DATABASE,
@@ -14,6 +14,13 @@ use tauri::State;
 #[derive(Serialize)]
 pub struct GetStringPayload {
     pub value: String,
+    pub ok: bool,
+}
+
+#[derive(Serialize)]
+pub struct GetTypeOfKeyPayload {
+    #[serde(rename = "keyType")]
+    pub key_type: String,
     pub ok: bool,
 }
 
@@ -133,6 +140,33 @@ pub async fn delete_all_keys(
         let response = connection.storage_client.delete_all_keys(request).await;
         match response {
             Ok(_response) => return Ok(()),
+            Err(err) => return Err(format!("{}", err)),
+        }
+    } else {
+        return Err(NO_CONNECTION_FOUND_MSG.to_string());
+    }
+}
+
+#[tauri::command]
+pub async fn get_type_of_key(
+    connection: State<'_, GrpcConnection>,
+    db_name: &str,
+    key: &str,
+) -> Result<GetTypeOfKeyPayload, String> {
+    let mut guard = connection.connection.lock().await;
+    if let Some(ref mut connection) = *guard {
+        let mut request = tonic::Request::new(GetTypeOfKeyRequest { key: key.to_owned() });
+        request
+            .metadata_mut()
+            .insert(MD_KEY_DATABASE, db_name.parse().unwrap());
+        let response = connection.storage_client.get_type_of_key(request).await;
+        match response {
+            Ok(response) => {
+                return Ok(GetTypeOfKeyPayload {
+                    key_type: response.get_ref().key_type.to_owned(),
+                    ok: response.get_ref().ok,
+                });
+            },
             Err(err) => return Err(format!("{}", err)),
         }
     } else {
