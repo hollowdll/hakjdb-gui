7c95c497d1fda915a019945b341b410069628c9d
hollowdll <pixerchat@gmail.com>
2024-08-30 11:13:22 +0300

    add settings module

---
+++ src-tauri/src/fs.rs
@@ -0,0 +1,2 @@
+pub const SETTINGS_FILE_NAME: &str = "hakjdb-gui.yaml";
+pub const SUBDIR_NAME: &str = "hakjdb-gui";
---
+++ src-tauri/src/settings.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct AppSettings {
+    /// Theme can be 'light' or 'dark'.
+    pub theme: String,
+}
+
+pub fn serialize_settings_toml(settings: &AppSettings) -> Result<String, toml::ser::Error> {
+    let toml = toml::to_string(settings)?;
+    Ok(toml)
+}
+
+pub fn deserialize_settings_toml(toml: &str) -> Result<AppSettings, toml::de::Error> {
+    let settings: AppSettings = toml::from_str(toml)?;
+    Ok(settings)
+}
